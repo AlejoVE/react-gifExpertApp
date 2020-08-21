0e3229dda85e310d40e2b4f89b4b495960689a14
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2020-08-21 21:55:03 +0200

    docs added

---
+++ docs/precache-manifest.20707fbd28e279c8a1584c9920f6b3be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0a125fd4466b98517c92270b4222a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a533ef10d70f1b8b9c3e",
+    "url": "/static/css/main.d3901462.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a533ef10d70f1b8b9c3e",
+    "url": "/static/js/main.eda2a593.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
